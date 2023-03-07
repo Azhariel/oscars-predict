9ed9cf008a0a41edd8594cf7b5af465a5a53321f
Azhariel <Azhariel@users.noreply.github.com>
2023-03-07 20:06:46 -0300

    ✨ render each movie with number of nominations

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script>
     import everythingPoster from "../images/everything-everywhere-all-at-once.webp"
-    import { movieStore } from "../../stores/MovieStore";
+    import { getUniqueMovies, getMovieNominations } from "../../stores/MovieStore";
 
-    console.log([...$movieStore]);
+    const movies = getUniqueMovies()
 </script>
 <svelte:head>
     <title>Oscars²³</title>
@@ -10,19 +10,27 @@
 <div class="navbar bg-base-100">
     <a href="/" class="btn btn-ghost normal-case text-xl">Oscars²³</a>
 </div>
-<div class="container">
-<div class="card w-96 bg-base-100 shadow-xl">
-    <figure><img src="{everythingPoster}" alt="Everything Everywhere All at Once" /></figure>
+
+<div class="flex flex-wrap p-8 w-auto justify-center">
+{#each movies as movie}
+  {#await getMovieNominations(movie) then nominations}
+
+<div class="card w-64 bg-base-100 shadow-xl mx-4">
+    <figure><img src="{everythingPoster}" alt="{movie}" /></figure>
     <div class="card-body">
-      <h2 class="card-title">
-        Shoes!
-        <div class="badge badge-secondary">NEW</div>
-      </h2>
-      <p>If a dog chews shoes whose shoes does he choose?</p>
+      <h1 class="card-title">
+        {movie}
+      </h1>
+      <p>Nominated for {nominations.length} {nominations.length == 1 ? 'award' : 'awards'}</p>
       <div class="card-actions justify-end">
-        <div class="badge badge-outline">Fashion</div> 
-        <div class="badge badge-outline">Products</div>
+        {#each nominations as nomination}
+        <div class="tooltip" data-tip="{nomination.nominated}">
+        <div class="badge badge-sm truncate">{nomination.categories[0]}</div> 
+    </div>
+        {/each}
       </div>
     </div>
   </div>
+  {/await}
+{/each}
 </div>
