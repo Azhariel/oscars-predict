075aa26e4fc02aaceff6c19fe4b53efdadd913e9
Azhariel <Azhariel@users.noreply.github.com>
2023-03-08 00:50:33 -0300

    🐛 update posters import for prod

--- stores/PosterStore.ts
+++ stores/PosterStore.ts
@@ -1,7 +1,7 @@
 import { writable } from 'svelte/store'
 import type { Writable } from 'svelte/store'
 import { getUniqueMovies } from './MovieStore'
-
+const images = import.meta.glob('../src/images/*.jpg', { eager: true, as: 'url' })
 export interface Poster {
     movie: string
     url: string
@@ -12,10 +12,17 @@ export const posterStore: Writable<Poster[]> = writable([])
 
 const setPosterStore = () => {
     const posters: Poster[] = []
-    movies.forEach((movie) => {
-        const path = `../src/images/${movie}.jpg`.replace('?','').replace(':','')
-        const url = new URL(path, import.meta.url).href
-        posters.push({ movie, url })
+    const regex = /\/([A-z,0-9 \u00C0-\u00FF]+)+\./g
+    movies.forEach(async (movie) => {
+        Object.entries(images).forEach((image) => {
+            const match = [...image[0].matchAll(regex)]
+            if (match[0][1]) {
+                const filename = match[0][1].replace('?', '').replace(':', '').replace("'", "")
+                if (filename.toLowerCase() === movie.toLowerCase().replace('?', '').replace(':', '').replace("'", "")) {
+                    posters.push({ movie, url: image[1] })
+                }
+            }
+        })
     })
     posterStore.set(posters)
 }
